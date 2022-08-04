936136245a81f73ea6691754fbfa00b23b9e3761
cynewulf-7 <106197878+cynewulf-7@users.noreply.github.com>
2022-08-04 12:42:47 +0100

    work in process

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Menu from './components/Menu';
 import Navbar from './components/Navbar';
 import { darkTheme, lightTheme } from "./utils/Theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Video from "./pages/Video";
+import SignIn from './pages/SignIn';
 
 const Container = styled.div`
   display: flex;
@@ -14,25 +17,34 @@ const Main = styled.div`
 `;
 
 const Wrapper = styled.div`
+  padding: 22px 96px
 `;
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+  <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
     <Container>
-      <BrowserRouter>
+     <BrowserRouter>
      <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
      <Main>
       <Navbar />
-      <Wrapper>
-       
+       <Wrapper>
+        <Routes>
+          <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="video">
+          <Route path=":id" element={<Video />} />  
+          </Route>
+          </Route>
+        </Routes>
       </Wrapper>
      </Main>
-     </BrowserRouter>
-    </Container>
-    </ThemeProvider>
+    </BrowserRouter>
+  </Container>
+ </ThemeProvider>
   );
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -17,6 +17,7 @@ import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined';
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
     flex: 1;
@@ -85,10 +86,12 @@ const Menu = ({darkMode, setDarkMode}) => {
   return (
     <Container>
         <Wrapper>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
             <Logo>
                 <Img src={Dynamo} />
                 CynewolfTube
             </Logo>
+        </Link>
             <Item>
                 <HomeIcon />
                 Home
