60513e620586e6c717087e4b6db313fa74b23928
cynewulf-7 <106197878+cynewulf-7@users.noreply.github.com>
2022-08-06 22:16:39 +0100

    backend beginning

---
+++ video_app/server/index.js
@@ -0,0 +1,24 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import dotenv  from "dotenv";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+const connect = () =>{
+    mongoose.connect(process.env.MONGO)
+    .then(()=>{
+    console.log("Connected to DB");
+    })
+    .catch((err) => {
+        throw err;
+    });
+};
+
+app.use("/api/users", userRoutes)
+
+app.listen(8800,()=>{
+    connect()
+    console.log("Connected to server");
+});
